5cec90795124e0824619dd89781fd9ba63e021b4
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2020-03-28 02:12:52 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# discord-diff-watch-bot
+
+License: MIT
---
+++ index.ts
@@ -0,0 +1,6 @@
+import * as diff from 'diff'
+import { Client } from 'discord.js'
+import config from './config.json'
+
+const client = new Client()
+console.log(config.watchInterval)
